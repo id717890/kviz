00752b542dc57c3ca8d02f999f51e068df714503
YusupovZA <jusupovz@gmail.com>
2021-03-28 10:39:54 +0500

    fix start + animation

--- pages/start.vue
+++ pages/start.vue
@@ -1,31 +1,30 @@
 <template>
-  <div id="neiros_kviz" class="start-wrapper">
-    <div class="start-image">
-      <v-img class="start-picture" src="/images/bg-left-kviz.jpg" cover></v-img>
-    </div>
-    <div class="start-content">
-      <div v-if="title" class="neiros__kviz_block_right_text_top">
-        {{ title }}
-        <p></p>
-        <!-- Пройдите тест и узнайте какие инструменты<br />
-        использовать для увеличения продаж
-        <p>В конце теста бонус - <b>профессиональная консультация</b></p> -->
-      </div>
-      <div v-if="subtitle" class="neiros__kviz_block_right_text_top">
-        {{ subtitle }}
-        <p></p>
-      </div>
-      <nuxt-link to="/quiz" class="neiros_kviz_btn">{{ buttonText }}</nuxt-link>
-      <div v-if="isShowBonus">
+  <div id="neiros_kviz" class="neiros_kviz">
+    <div
+      class="neiros__kviz_block_left scale-up-hor-left"
+      style="
+        background: url(images/bg-left-kviz.jpg) center center no-repeat;
+        background-size: cover;
+      "
+    ></div>
+    <div class="neiros__kviz_block_right scale-up-hor-right d-flex">
+      <div
+        class="neiros__kviz_padding_block py-0 d-flex flex-column justify-content-center flex-grow-1"
+      >
+        <div v-if="title" class="neiros__kviz_block_right_text_top">
+          {{ subtitle }}
+          <p></p>
+        </div>
+
+        <nuxt-link to="/quiz" class="neiros_kviz_btn">
+          {{ buttonText }}
+        </nuxt-link>
+
         <div class="txt1">Бонус после прохождения теста!</div>
         <Bonus />
-        <!-- <div class="neiros__bonus-container">
-          <img src="images/bonus-img.PNG" alt="" />
-          <div>Полное<br />руковоство<br />для бизнеса</div>
-        </div> -->
-      </div>
-      <div class="neiros__footer">
-        <span>Сделано в</span> <img src="/images/logo-kviz.PNG" />
+        <div class="neiros__footer">
+          <span>Сделано в</span> <img src="images/logo-kviz.PNG" />
+        </div>
       </div>
     </div>
   </div>
